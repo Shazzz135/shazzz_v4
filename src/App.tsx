06bc689d6d8fc17f4582b9cda36811a1fd7e57e4
Nick Shahbaz <nickshahbaz135@gmail.com>
2026-09-18 13:26:44 -0400

    good job

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function RouteTransition() {
 
   useEffect(() => {
     if (location.pathname !== displayLocation.pathname) {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
       setIsTransitioning(true);
       const timer = setTimeout(() => {
         setDisplayLocation(location);
